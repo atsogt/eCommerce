23f4a49675caf7ab517a2602e5391602329b82a6
Anand Tsogtbaatar <48449043+atsogt@users.noreply.github.com>
2024-10-15 23:40:18 -0400

    Update collection-item.component.jsx

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,6 +5,7 @@ const CollectionItem = ({ name, imageUrl, price }) => (
   <div className='collection-item'>
     <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className='collection-footer'>
+      //fetch item object from endpoint
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
